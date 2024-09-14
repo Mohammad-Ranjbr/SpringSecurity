828cccb3f0199b1085d98670ed3d5a60c3ed3ce0
Mohammad-Ranjbr <mohammadranjbar.mmr81@gmail.com>
2024-09-14 16:58:18 +0330

    Configuring users using InMemoryUserDetailsManager

--- src/main/java/com/example/SpringSecurity/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/SpringSecurity/config/ProjectSecurityConfig.java
@@ -3,6 +3,10 @@ package com.example.SpringSecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -67,6 +71,13 @@ public class ProjectSecurityConfig {
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withUsername("user").password("{noop}123456").authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}654321").authorities("admin").build();
+        return new InMemoryUserDetailsManager(user,admin);
     }
 
 }
