4fc1f06fee402fc242aadc79260bc79c13bd1ff5
Mohammad-Ranjbr <mohammadranjbar.mmr81@gmail.com>
2024-09-29 12:42:14 +0330

    edit BalanceController

--- src/main/java/com/example/SpringSecurity/controller/BalanceController.java
+++ src/main/java/com/example/SpringSecurity/controller/BalanceController.java
@@ -1,14 +1,23 @@
 package com.example.SpringSecurity.controller;
 
+import com.example.SpringSecurity.model.AccountTransactions;
+import com.example.SpringSecurity.repository.AccountTransactionsRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class BalanceController {
 
+    private final AccountTransactionsRepository accountTransactionsRepository;
+
     @GetMapping("/myBalance")
-    public String getBalance(){
-        return "Here are the balance details from the DB";
+    public List<AccountTransactions> getBalanceDetails(@RequestParam long id){
+        return accountTransactionsRepository.findByCustomerIdOrderByTransactionDtDesc(id);
     }
 
 }
