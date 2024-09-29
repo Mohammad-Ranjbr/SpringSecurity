8e894acc92e19ceeb191baa3452bdc8365b9bd4c
Mohammad-Ranjbr <mohammadranjbar.mmr81@gmail.com>
2024-09-29 13:19:39 +0330

    edit LoansController

--- src/main/java/com/example/SpringSecurity/controller/LoansController.java
+++ src/main/java/com/example/SpringSecurity/controller/LoansController.java
@@ -1,14 +1,23 @@
 package com.example.SpringSecurity.controller;
 
+import com.example.SpringSecurity.model.Loans;
+import com.example.SpringSecurity.repository.LoanRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class LoansController {
 
+    private final LoanRepository loanRepository;
+
     @GetMapping("/myLoans")
-    public String getLoans(){
-        return "Here are the loans details from the DB";
+    public List<Loans> getLoansDetails(@RequestParam long id){
+        return loanRepository.findByCustomerIdOrderByStartDtDesc(id);
     }
 
 }
