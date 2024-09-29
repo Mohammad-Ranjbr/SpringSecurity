a75a181c0aa38e6e1d941ffc6666c6bb3647d20f
Mohammad-Ranjbr <mohammadranjbar.mmr81@gmail.com>
2024-09-29 13:28:10 +0330

    edit NoticeController

--- src/main/java/com/example/SpringSecurity/controller/NoticeController.java
+++ src/main/java/com/example/SpringSecurity/controller/NoticeController.java
@@ -1,14 +1,39 @@
 package com.example.SpringSecurity.controller;
 
+import com.example.SpringSecurity.model.Notice;
+import com.example.SpringSecurity.repository.NoticeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 @RestController
+@RequiredArgsConstructor
 public class NoticeController {
 
+    private final NoticeRepository noticeRepository;
+
     @GetMapping("/notices")
-    public String getNotices(){
-        return "Here are the notices details from the DB";
+    public ResponseEntity<List<Notice>> getNotices(){
+        List<Notice> notices = noticeRepository.findAllActiveNotices();
+        if(notices != null){
+            return ResponseEntity.ok()
+                    // This section adds the Cache-Control header to the response.
+                    // .cacheControl() allows you to specify how to cache responses.
+                    // CacheControl.maxAge(60, TimeUnit.SECONDS) says that this
+                    // response should be kept in the browser or server cache for 60 seconds.
+                    // This means that after receiving this response, the browser or
+                    // proxy server can save this response for 60 seconds
+                    // and prevent the server from sending the request again.
+                    .cacheControl(CacheControl.maxAge(60, TimeUnit.SECONDS))
+                    .body(notices);
+        } else {
+            return null;
+        }
     }
 
 }
