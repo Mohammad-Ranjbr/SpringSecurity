ec8e2c01896575e2d8253d2f3cb76748d7de48a4
Mohammad-Ranjbr <mohammadranjbar.mmr81@gmail.com>
2024-09-29 12:45:04 +0330

    edit CardsRepository

--- src/main/java/com/example/SpringSecurity/controller/CardsController.java
+++ src/main/java/com/example/SpringSecurity/controller/CardsController.java
@@ -1,14 +1,23 @@
 package com.example.SpringSecurity.controller;
 
+import com.example.SpringSecurity.model.Cards;
+import com.example.SpringSecurity.repository.CardsRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class CardsController {
 
+    private final CardsRepository cardsRepository;
+
     @GetMapping("/myCards")
-    public String getCards(){
-        return "Here are the cards details from the DB";
+    public List<Cards> getCardsDetails(@RequestParam long id){
+        return cardsRepository.findByCustomerId(id);
     }
 
 }
