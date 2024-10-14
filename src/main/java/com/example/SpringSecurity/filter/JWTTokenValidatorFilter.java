2457fe307c7456a5560e4a42efc848f32ac96281
Mohammad Ranjbar <mohammadranjbar.mmr81@gmail.com>
2024-10-14 17:28:52 +0330

    Validating the JWT changes made by running the applications

--- src/main/java/com/example/SpringSecurity/filter/JWTTokenValidatorFilter.java
+++ src/main/java/com/example/SpringSecurity/filter/JWTTokenValidatorFilter.java
@@ -9,6 +9,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -35,21 +36,25 @@ public class JWTTokenValidatorFilter extends OncePerRequestFilter {
                                     throws ServletException, IOException {
 
         String jwt = request.getHeader(ApplicationConstants.JWT_HEADER);
-        if(jwt != null){
-            Environment env = getEnvironment();
-            if(env != null){
-                String secret = env.getProperty(ApplicationConstants.JWT_SECRET_KEY,
-                        ApplicationConstants.JWT_SECRET_DEFAULT_VALUE);
-                SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
-                if(secretKey != null){
-                    Claims claims = Jwts.parser().verifyWith(secretKey)
-                            .build().parseSignedClaims(jwt).getPayload();
-                    String username =String.valueOf( claims.get("username"));
-                    String authorities = String.valueOf(claims.get("authorities")); // authorities : as string separate with comma
-                    Authentication authentication = new UsernamePasswordAuthenticationToken(username, null,
-                            AuthorityUtils.commaSeparatedStringToAuthorityList(authorities));
-                    SecurityContextHolder.getContext().setAuthentication(authentication);
+        if(null != jwt) {
+            try {
+                Environment env = getEnvironment();
+                if (null != env) {
+                    String secret = env.getProperty(ApplicationConstants.JWT_SECRET_KEY,
+                            ApplicationConstants.JWT_SECRET_DEFAULT_VALUE);
+                    SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+                    if(null !=secretKey) {
+                        Claims claims = Jwts.parser().verifyWith(secretKey)
+                                .build().parseSignedClaims(jwt).getPayload();
+                        String username = String.valueOf(claims.get("username"));
+                        String authorities = String.valueOf(claims.get("authorities"));
+                        Authentication authentication = new UsernamePasswordAuthenticationToken(username, null,
+                                AuthorityUtils.commaSeparatedStringToAuthorityList(authorities));
+                        SecurityContextHolder.getContext().setAuthentication(authentication);
+                    }
                 }
+            } catch (Exception exception) {
+                throw new BadCredentialsException("Invalid Token received!");
             }
         }
         filterChain.doFilter(request,response);
@@ -57,7 +62,7 @@ public class JWTTokenValidatorFilter extends OncePerRequestFilter {
 
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-        return request.getServletPath().equals("/user");
+        return request.getServletPath().equals("/user"); // Login API
     }
 
 }
